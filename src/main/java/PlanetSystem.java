a7f119bbfbb3527c1d33102dbcca85877a6daba2
Mika1212 <54846851+Mika1212@users.noreply.github.com>
2021-05-16 20:03:56 +0300

    It is alive!

--- src/main/java/PlanetSystem.java
+++ src/main/java/PlanetSystem.java
@@ -16,15 +16,14 @@ public class PlanetSystem {
     static int scaleChange = 0;
     static int horizontalChange = 0;
     static int verticalChange = 0;
-    static SpaceObjects.Star star = new SpaceObjects.Star(550, 500, 80, Color.CYAN);
 
 
     //Method to find coordinates x,y of a planet
     public static void coordinates(SpaceObjects.Planet planet, int i) {
         double psi = (i * Math.PI / 180 / planet.remoteness);
         double fi = Math.atan2(planet.a * Math.sin(psi), planet.b * Math.cos(psi));
-        planet.y = planet.a * Math.cos(fi) + 455;
-        planet.x = planet.b * Math.sin(fi) + 580;
+        planet.y = planet.a * Math.cos(fi) + 450 - planet.size / 2 + 25;
+        planet.x = planet.b * Math.sin(fi) + 575 - planet.size / 2 + 25;
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -40,34 +39,28 @@ public class PlanetSystem {
 
         //buttons
         boolean[] pause = {false};
-        Button pauseButton = new Button("Pause");
+        Button pauseButton = new Button("Play/Pause");
         pauseButton.setBounds(70, 100, 110,30);
         pauseButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                pause[0] = true;
-            }
-        });
-
-
-        Button playButton = new Button("Play");
-        playButton.setBounds(70, 200, 110,30);
-        playButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+                if (pause[0])
                 pause[0] = false;
+                else pause[0] = true;
             }
         });
+
 
         int[] speed = {10};
-        Button speedX05 = new Button("Click to speed down");
-        speedX05.setBounds(0, 300, 110,30);
+        Button speedX05 = new Button("Slower");
+        speedX05.setBounds(10, 200, 110,30);
         speedX05.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (speed[0] < 100) speed[0] += 3;
             }
         });
 
-        Button speedX2 = new Button("Click to speed up");
-        speedX2.setBounds(120, 300,110,30);
+        Button speedX2 = new Button("Faster");
+        speedX2.setBounds(130, 200,110,30);
         speedX2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (speed[0] > 10 ) speed[0] -= speed[0] / 10;
@@ -75,8 +68,8 @@ public class PlanetSystem {
             }
         });
 
-        Button speedNormal = new Button("Back to normal");
-        speedNormal.setBounds(70, 340, 110,30);
+        Button speedNormal = new Button("Normal speed");
+        speedNormal.setBounds(70, 240, 110,30);
         speedNormal.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 speed[0] = 10;
@@ -92,7 +85,7 @@ public class PlanetSystem {
         });
 
         Button scaleDownButton = new Button("ScaleDown");
-        scaleDownButton.setBounds(10, 440, 110,30);
+        scaleDownButton.setBounds(130, 400, 110,30);
         scaleDownButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 if (scaleChange == 1) {
@@ -104,7 +97,7 @@ public class PlanetSystem {
         });
 
         Button rightButton = new Button("Move right");
-        rightButton.setBounds(10, 480, 110,30);
+        rightButton.setBounds(130, 520, 110,30);
         rightButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 if (pointOfView[2] + 100 <= 800) {
@@ -124,7 +117,7 @@ public class PlanetSystem {
         });
 
         Button upButton = new Button("Move up");
-        upButton.setBounds(10, 560, 110,30);
+        upButton.setBounds(70, 480, 110,30);
         upButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 if (pointOfView[1] - 100 > 0) {
@@ -134,7 +127,7 @@ public class PlanetSystem {
         });
 
         Button downButton = new Button("Move down");
-        downButton.setBounds(10, 600, 110,30);
+        downButton.setBounds(70, 560, 110,30);
         downButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 if (pointOfView[3] + 100 <= 1000) {
@@ -143,9 +136,7 @@ public class PlanetSystem {
             }
         });
 
-
         buttons.add(pauseButton);
-        buttons.add(playButton);
         buttons.add(speedX05);
         buttons.add(speedX2);
         buttons.add(speedNormal);
@@ -162,19 +153,18 @@ public class PlanetSystem {
 
 
         //All planets (I can change it to .txt file)
-        allPlanets.add(new SpaceObjects.Planet(75, 150, 40, 1, Color.BLUE));
-        allPlanets.add(new SpaceObjects.Planet(150, 225, 50, 2, Color.ORANGE));
-        allPlanets.add(new SpaceObjects.Planet(225, 300, 50, 3, Color.PINK));
-        allPlanets.add(new SpaceObjects.Planet(300, 375, 50, 4, Color.CYAN));
+        allPlanets.add(new SpaceObjects.Planet(75, 150, 50, 1, Color.BLUE));
+        allPlanets.add(new SpaceObjects.Planet(150, 225, 20, 2, Color.ORANGE));
+        allPlanets.add(new SpaceObjects.Planet(225, 300, 30, 3, Color.PINK));
+        allPlanets.add(new SpaceObjects.Planet(300, 375, 40, 4, Color.CYAN));
         allPlanets.add(new SpaceObjects.Planet(375, 450, 50, 5, Color.DARK_GRAY));
-        allPlanets.add(new SpaceObjects.Planet(450, 525, 50, 6, Color.GREEN));
+        allPlanets.add(new SpaceObjects.Planet(450, 525, 60, 6, Color.GREEN));
 
         //Run of the process of painting
         int i = 0;
         while (true) {
 
             if (!pause[0]) {
-                int j = 0;
                 i++;
                 for (SpaceObjects.Planet planet : allPlanets) coordinates(planet, i);
 
